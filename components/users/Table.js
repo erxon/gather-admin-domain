2b43c441c9d9bc705fa3967ca9664bb38025a205
Ericson Castasus <castasuse@gmail.com>
2023-06-21 22:40:30 +0800

    view users

---
+++ components/users/Table.js
@@ -0,0 +1,118 @@
+"use client";
+import { Box, Button, Typography, Stack, Chip } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+//Imports #Icons
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
+import PeopleIcon from "@mui/icons-material/People";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import InfoIcon from "@mui/icons-material/Info";
+
+//Imports #Hooks
+import Router from "next/router";
+
+const columns = [
+  { field: "id", headerName: "Index", width: 50 },
+  {
+    field: "_id",
+    headerName: "Database ID",
+    width: 350,
+    renderCell: (params) => (
+      <Stack direction="row" spacing={1}>
+        <Typography sx={{ width: 250 }} variant="subtitle1">
+          {params.value}
+        </Typography>
+        <Button
+          onClick={() => {
+            Router.push(
+              `https://gather-plum.vercel.app/profile/${params.value}`
+            );
+          }}
+          size="small"
+          variant="contained"
+        >
+          View
+        </Button>
+      </Stack>
+    ),
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 150,
+    renderCell: (params) =>
+      params.value === "verified" ? (
+        <Chip icon={<CheckCircleIcon />} color="success" label={params.value} />
+      ) : (
+        <Chip icon={<InfoIcon />} color="info" label={params.value} />
+      ),
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 150,
+    renderCell: (params) =>
+      params.value === "authority" ? (
+        <Chip
+          icon={<LocalPoliceIcon />}
+          color="secondary"
+          label={params.value}
+        />
+      ) : (
+        <Chip icon={<PeopleIcon />} color="primary" label={params.value} />
+      ),
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "contactNumber",
+    headerName: "Contact Number",
+    width: 150,
+    editable: true,
+  },
+];
+
+export default function Table(props) {
+  console.log(props.users);
+  return (
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        autoHeight
+        rows={props.users}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
---
+++ pages/users/index.js
@@ -0,0 +1,40 @@
+
+//Import #Material UI
+import { Box, CircularProgress, Typography } from "@mui/material";
+
+//Import #Hooks
+import useSWR from "swr";
+import { fetcher } from "@/utils/auth/hooks";
+
+//Import #Components
+import Table from "@/components/users/Table";
+
+function addID(data){
+    let index = -1
+    const newArray = data.map((document) => {
+        index = index + 1;
+        return {...document, id: index }
+    })
+
+    return newArray;
+
+}
+
+export default function Page() {
+  //fetch all users
+  const { data, mutate, error, isLoading } = useSWR("/api/users", fetcher);
+
+  if (error)
+    return <Typography>Something went wrong while fetching users.</Typography>;
+  if (isLoading) return <CircularProgress />;
+
+  const result = addID(data);
+
+  return (
+    <div>
+      <Box>
+        <Table users={result} />
+      </Box>
+    </div>
+  );
+}
